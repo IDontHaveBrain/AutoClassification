3cfb5d473d65cdb47c43f001aec97c7bad6bab58
nj.jo <jonamjun.dev@gmail.com>
2024-02-27 10:47:29 +0900

    fix: 파일 업로드 버그 수정

--- UserServer/src/main/java/cc/nobrain/dev/userserver/common/component/FileComponent.java
+++ UserServer/src/main/java/cc/nobrain/dev/userserver/common/component/FileComponent.java
@@ -1,5 +1,6 @@
 package cc.nobrain.dev.userserver.common.component;
 
+import cc.nobrain.dev.userserver.common.properties.AppProps;
 import cc.nobrain.dev.userserver.domain.base.entity.File;
 import cc.nobrain.dev.userserver.domain.base.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,13 @@ public class FileComponent {
 
     private final ModelMapper modelMapper;
     private final FileRepository fileRepository;
-    private final String uploadPath = "/path/to/your/upload/directory/";
+    private final AppProps appProps;
 
     public <T extends File> List<T> uploadFile(MultipartFile[] files, Class<T> clazz, Object ownerEntity) {
         List<T> result = new ArrayList<>();
+        if (Objects.isNull(files) || files.length < 1) {
+            return result;
+        }
         for (MultipartFile file : files) {
             Optional<T> uploadedFile = uploadFile(file, clazz, ownerEntity);
             uploadedFile.ifPresent(result::add);
@@ -32,26 +36,27 @@ public class FileComponent {
         return result;
     }
 
-    private <T extends File> Optional<T> uploadFile(MultipartFile file, Class<T> clazz, Object ownerEntity) {
+    protected <T extends File> Optional<T> uploadFile(MultipartFile file, Class<T> clazz, Object ownerEntity) {
         try {
+            if (file.isEmpty() || file.getSize() > appProps.getMaxFileSize()) {
+                return Optional.empty();
+            }
+
             String filename = file.getOriginalFilename();
             long size = file.getSize();
             String contentType = file.getContentType();
 
-            Path filePath = Paths.get(uploadPath + filename);
+            Path filePath = Paths.get(appProps.getPath() + filename);
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
-            //Preparing a source map with file details
             Map<String, Object> sourceMap = new HashMap<>();
             sourceMap.put("path", filePath.toString());
             sourceMap.put("size", size);
             sourceMap.put("mimeType", contentType);
             sourceMap.put("fileName", filename);
 
-            //Using ModelMapper to map the source map to the instance of File
             T uploadedFile = modelMapper.map(sourceMap, clazz);
             uploadedFile.setRelation(ownerEntity);
-//            fileRepository.save(uploadedFile);
 
             return Optional.ofNullable(uploadedFile);
         } catch (IOException e) {
---
+++ UserServer/src/main/java/cc/nobrain/dev/userserver/common/properties/AppProps.java
@@ -0,0 +1,13 @@
+package cc.nobrain.dev.userserver.common.properties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "app.storage")
+@RequiredArgsConstructor
+public class AppProps {
+    private final String path;
+    private final Long maxFileSize;
+}
