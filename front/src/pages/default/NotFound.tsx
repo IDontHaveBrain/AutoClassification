814e9858155d1f5d2177a6bc4bae5134d68a5f78
nj.jo <jonamjun.dev@gmail.com>
2024-03-23 16:01:06 +0900

    style: 404 페이지 UI 수정

--- front/src/pages/default/NotFound.tsx
+++ front/src/pages/default/NotFound.tsx
@@ -1,12 +1,25 @@
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const NotFound = () => {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            textAlign="center"
+        >
+            <Typography variant="h2" gutterBottom>
+                404 - Not Found
+            </Typography>
+            <Typography variant="subtitle1">
+                Sorry, the page you are looking for does not exist.
+            </Typography>
+            <Button sx={{ mt: 3 }} variant="contained" color="primary" component={Link} to="/">
+                Go to Home Page
+            </Button>
+        </Box>
+    );
 };
