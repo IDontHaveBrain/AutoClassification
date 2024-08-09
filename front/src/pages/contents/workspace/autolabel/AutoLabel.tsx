ac2cb162718a5c365dc498090736b5e09b922ca3
nj.jo <jonamjun.dev@gmail.com>
2024-08-09 20:10:31 +0900

    feat: AutoLabel 컴포넌트의 UI 개선 및 로딩 상태 추가

--- front/src/pages/contents/workspace/autolabel/AutoLabel.tsx
+++ front/src/pages/contents/workspace/autolabel/AutoLabel.tsx
@@ -1,41 +1,46 @@
-import {useEffect, useState} from "react";
-import {getMyWorkspaceList, getWorkspace} from "service/Apis/WorkspaceApi";
-import {WorkspaceModel} from "model/WorkspaceModel";
-import {Autocomplete, Button} from "@mui/material";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import React, { useEffect, useState } from "react";
+import { getMyWorkspaceList, getWorkspace } from "service/Apis/WorkspaceApi";
+import { WorkspaceModel } from "model/WorkspaceModel";
+import { Autocomplete, Button, TextField, Grid, Paper, Typography, CircularProgress } from "@mui/material";
 import BaseTitle from "component/baseBoard/BaseTitle";
-import {onAlert} from "component/modal/AlertModal";
+import { onAlert } from "component/modal/AlertModal";
 import WorkspaceDataSet from "pages/contents/workspace/editor/WorkspaceDataSet";
-import {Strings} from "utils/strings";
+import { Strings } from "utils/strings";
 import LabelledImages from "component/imgs/LabelledImages";
-import {blue} from "@mui/material/colors";
-import {requestAutoLabel} from "service/Apis/TrainApi";
+import { requestAutoLabel } from "service/Apis/TrainApi";
 
-const AutoLabel = () => {
+const AutoLabel: React.FC = () => {
     const [workspaceList, setWorkspaceList] = useState<WorkspaceModel[]>([]);
     const [selected, setSelected] = useState<WorkspaceModel | null>(null);
-
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
-        getMyWorkspaceList({size: 100}).then(res => {
-            setWorkspaceList(res.data.content);
-        }).catch(err => {
-            console.error(err);
-        });
-    }, []);
-
-    const isOptionEqualToValue = (option, value) => option.id === value.id;
-
-    const handleSelectChange = (e, newValue: WorkspaceModel) => {
-        const workspace = newValue;
-        if (workspace) {
-            getWorkspace(workspace?.id).then(res => {
-                setSelected(res.data);
-            }).catch(err => {
+        setIsLoading(true);
+        getMyWorkspaceList({size: 100})
+            .then(res => {
+                setWorkspaceList(res.data.content);
+            })
+            .catch(err => {
                 console.error(err);
                 onAlert(Strings.Common.apiFailed);
-            });
+            })
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    const handleSelectChange = (e: React.ChangeEvent<{}>, newValue: WorkspaceModel | null) => {
+        if (newValue) {
+            setIsLoading(true);
+            getWorkspace(newValue.id)
+                .then(res => {
+                    setSelected(res.data);
+                })
+                .catch(err => {
+                    console.error(err);
+                    onAlert(Strings.Common.apiFailed);
+                })
+                .finally(() => setIsLoading(false));
+        } else {
+            setSelected(null);
         }
     };
 
@@ -49,41 +54,62 @@ const AutoLabel = () => {
             return;
         }
 
-        requestAutoLabel(selected.id).then(res => {
-            onAlert(Strings.Common.apiSuccess);
-        }).catch(err => {
-            console.error(err);
-            onAlert(Strings.Common.apiFailed);
-        });
+        setIsLoading(true);
+        requestAutoLabel(selected.id)
+            .then(() => {
+                onAlert(Strings.Common.apiSuccess);
+            })
+            .catch(err => {
+                console.error(err);
+                onAlert(Strings.Common.apiFailed);
+            })
+            .finally(() => setIsLoading(false));
     }
 
     return (
-        <>
+        <Paper elevation={3} sx={{ p: 3, m: 2 }}>
             <BaseTitle title={'AutoLabel'}/>
-            <Grid container direction="row" alignItems="center" spacing={2}>
-                <Grid item xs={3}>
-                    <Autocomplete
-                        options={workspaceList}
-                        getOptionLabel={(option) => option.name}
-                        value={selected}
-                        isOptionEqualToValue={isOptionEqualToValue}
-                        onChange={handleSelectChange}
-                        renderInput={(params) => <TextField {...params} label="Workspace" variant="outlined"/>}
-                    />
+            <Grid container direction="column" spacing={3}>
+                <Grid item container alignItems="center" spacing={2}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Autocomplete
+                            options={workspaceList}
+                            getOptionLabel={(option) => option.name}
+                            value={selected}
+                            onChange={handleSelectChange}
+                            renderInput={(params) => <TextField {...params} label="Workspace" variant="outlined"/>}
+                            disabled={isLoading}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button 
+                            onClick={handleLabelingRequest} 
+                            variant="contained" 
+                            color="primary"
+                            disabled={!selected || isLoading}
+                        >
+                            {isLoading ? <CircularProgress size={24} /> : "Labeling Request"}
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Button onClick={handleLabelingRequest} color="inherit" style={{ backgroundColor: blue[300] }}>Labeling Request</Button>
-                </Grid>
+                {selected && (
+                    <Grid item container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h6" gutterBottom>Workspace Data</Typography>
+                            <WorkspaceDataSet 
+                                imgs={selected.files || []} 
+                                setState={setSelected}
+                                isLoading={isLoading}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="h6" gutterBottom>Labelled Images</Typography>
+                            <LabelledImages files={selected.files || []}/>
+                        </Grid>
+                    </Grid>
+                )}
             </Grid>
-            <Grid mt={2} container spacing={2} xs={10}>
-                {selected && selected.files &&
-                    <>
-                        <WorkspaceDataSet imgs={selected?.files || []} setState={setSelected}/>
-                        <LabelledImages files={selected?.files || []}/>
-                    </>
-                }
-            </Grid>
-        </>
+        </Paper>
     );
 }
 
