9da50bbe8aaf837b3c925bc19974102f48998e81
nj.jo <jonamjun.dev@gmail.com>
2024-08-17 00:31:52 +0900

    feat: TRAIN_QUEUE 추가
    RabbitMQ에 TRAIN_QUEUE, TRAIN_EXCHANGE, TRAIN_RESPONSE_QUEUE 추가 및 바인딩 설정.

--- UserServer/src/main/kotlin/cc/nobrain/dev/userserver/common/config/RabbitMqConfiguration.kt
+++ UserServer/src/main/kotlin/cc/nobrain/dev/userserver/common/config/RabbitMqConfiguration.kt
@@ -14,6 +14,9 @@ class RabbitMqConfiguration {
         const val CLASSIFY_EXCHANGE = "ClassifyExchange"
         const val CLASSIFY_QUEUE = "ClassifyQueue"
         const val CLASSIFY_RESPONSE_QUEUE = "ClassifyResponseQueue"
+        const val TRAIN_EXCHANGE = "TrainExchange"
+        const val TRAIN_QUEUE = "TrainQueue"
+        const val TRAIN_RESPONSE_QUEUE = "TrainResponseQueue"
     }
 
     @Bean
@@ -32,6 +35,23 @@ class RabbitMqConfiguration {
     @Bean
     fun bindingClassifyResponseQueue(classifyResponseQueue: Queue, classifyExchange: FanoutExchange): Binding =
         BindingBuilder.bind(classifyResponseQueue).to(classifyExchange)
+
+    @Bean
+    fun trainExchange(): FanoutExchange = FanoutExchange(TRAIN_EXCHANGE)
+
+    @Bean
+    fun trainQueue(): Queue = Queue(TRAIN_QUEUE, true)
+
+    @Bean
+    fun trainResponseQueue(): Queue = Queue(TRAIN_RESPONSE_QUEUE, true)
+
+    @Bean
+    fun bindingTrainQueue(trainQueue: Queue, trainExchange: FanoutExchange): Binding =
+        BindingBuilder.bind(trainQueue).to(trainExchange)
+
+    @Bean
+    fun bindingTrainResponseQueue(trainResponseQueue: Queue, trainExchange: FanoutExchange): Binding =
+        BindingBuilder.bind(trainResponseQueue).to(trainExchange)
 
     @Bean
     fun rabbitTemplate(connectionFactory: ConnectionFactory): RabbitTemplate =
