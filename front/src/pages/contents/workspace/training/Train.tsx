0a5cc52c123766b78b2eff21a7d5946011a0bd9d
nj.jo <jonamjun.dev@gmail.com>
2024-08-16 22:35:19 +0900

    feat: Train 컴포넌트 디자인 개선 및 AutoLabel 컴포넌트와 일관성 유지
    Train 컴포넌트를 AutoLabel 컴포넌트와 일관성 있게 디자인하여 사용자 인터페이스와 경험을 개선했습니다.

--- front/src/pages/contents/workspace/training/Train.tsx
+++ front/src/pages/contents/workspace/training/Train.tsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
 import { WorkspaceModel } from "model/WorkspaceModel";
-import {getMyWorkspaceList, getWorkspace} from "service/Apis/WorkspaceApi";
+import { getMyWorkspaceList, getWorkspace } from "service/Apis/WorkspaceApi";
 import BaseTitle from "component/baseBoard/BaseTitle";
 import Grid from "@mui/material/Grid";
-import {Autocomplete, Button} from "@mui/material";
-import TextField from "@mui/material/TextField";
-import {blue} from "@mui/material/colors";
-import {onAlert} from "component/modal/AlertModal";
-import {Strings} from "utils/strings";
+import { Autocomplete, Button, TextField, Paper, Typography, CircularProgress } from "@mui/material";
+import { onAlert } from "component/modal/AlertModal";
+import { Strings } from "utils/strings";
 import LabelledImages from "component/imgs/LabelledImages";
+import WorkspaceDataSet from "pages/contents/workspace/editor/WorkspaceDataSet";
 
 const Train = () => {
     const [workspaceList, setWorkspaceList] = useState<WorkspaceModel[]>([]);
     const [selected, setSelected] = useState<WorkspaceModel | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
+        setIsLoading(true);
         getMyWorkspaceList({ size: 100 })
             .then((res) => {
                 setWorkspaceList(res.data.content);
             })
             .catch((err) => {
                 console.error(err);
-            });
+            })
+            .finally(() => setIsLoading(false));
     }, []);
 
     const isOptionEqualToValue = (option, value) => option.id === value.id;
@@ -29,12 +31,16 @@ const Train = () => {
     const handleSelectChange = (e, newValue: WorkspaceModel) => {
         const workspace = newValue;
         if (workspace) {
-            getWorkspace(workspace?.id).then(res => {
-                setSelected(res.data);
-            }).catch(err => {
-                console.error(err);
-                onAlert(Strings.Common.apiFailed);
-            });
+            setIsLoading(true);
+            getWorkspace(workspace?.id)
+                .then(res => {
+                    setSelected(res.data);
+                })
+                .catch(err => {
+                    console.error(err);
+                    onAlert(Strings.Common.apiFailed);
+                })
+                .finally(() => setIsLoading(false));
         }
     };
 
@@ -49,32 +55,52 @@ const Train = () => {
         }
 
         // TODO: 훈련 요청
-    }
+    };
 
     return (
-        <>
-            <BaseTitle title={'Train'}/>
-            <Grid container direction="row" alignItems="center" spacing={2}>
-                <Grid item xs={3}>
-                    <Autocomplete
-                        options={workspaceList}
-                        getOptionLabel={(option) => option.name}
-                        value={selected}
-                        isOptionEqualToValue={isOptionEqualToValue}
-                        onChange={handleSelectChange}
-                        renderInput={(params) => <TextField {...params} label="Workspace" variant="outlined"/>}
-                    />
+        <Paper elevation={3} sx={{ p: 3, m: 2 }}>
+            <BaseTitle title={'Train'} />
+            <Grid container direction="column" spacing={3}>
+                <Grid item container alignItems="center" spacing={2}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Autocomplete
+                            options={workspaceList}
+                            getOptionLabel={(option) => option.name}
+                            value={selected}
+                            onChange={handleSelectChange}
+                            renderInput={(params) => <TextField {...params} label="Workspace" variant="outlined" />}
+                            disabled={isLoading}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            onClick={handleTrainRequest}
+                            variant="contained"
+                            color="primary"
+                            disabled={!selected || isLoading}
+                        >
+                            {isLoading ? <CircularProgress size={24} /> : "Train Request"}
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Button onClick={handleTrainRequest} color="inherit" style={{ backgroundColor: blue[300] }}>Train Request</Button>
-                </Grid>
+                {selected && (
+                    <Grid item container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h6" gutterBottom>Workspace Data</Typography>
+                            <WorkspaceDataSet
+                                imgs={selected.files || []}
+                                setState={setSelected}
+                                isLoading={isLoading}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="h6" gutterBottom>Labelled Images</Typography>
+                            <LabelledImages files={selected.files || []} />
+                        </Grid>
+                    </Grid>
+                )}
             </Grid>
-            <Grid mt={2} container spacing={2} xs={10}>
-                {selected && selected.files &&
-                    <LabelledImages files={selected?.files || []}/>
-                }
-            </Grid>
-        </>
+        </Paper>
     );
 };
 
